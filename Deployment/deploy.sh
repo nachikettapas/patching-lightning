e53c9f32379ff4bbca03638c8e43db97765f4a7b
Nachiket Tapas <nachiket.tapas@ibm.com>
2019-12-25 16:55:04 +0200

    Few more fixes 1

--- Deployment/deploy.sh
+++ Deployment/deploy.sh
@@ -115,6 +115,8 @@ if [ "$NEW_INSTALL" = "1" ] && [ "$RBP" = "0" ]; then
            echo "Start install packages"
            ssh -n $target "cd ~/patching-lightning/ && npm install && cd node_modules/webtorrent/ && sudo rm -r node_modules/ && npm install && cd /home/$user/ && export LC_ALL=C && sudo apt install -y python3-pip && cd ~/patching-lightning/Utils/AddressGeneration/ && sudo pip3 install -r requirements.txt"
            echo "start copy bitcoind config file"
+		   echo "$bitcoinSource"
+		   echo "$bitcoinTarget"
            scp -r $bitcoinSource $bitcoinTarget
            #get lightning configuration
            if [ "$IOT" = "1" ]; then
@@ -141,7 +143,7 @@ if [ "$NEW_INSTALL" = "1" ] && [ "$RBP" = "0" ]; then
            elif [ "$VENDOR" = "1" ]; then
                echo "Start lightning channel setup"
                ssh -n $target "node /home/$user/patching-lightning/Deployment/createConfig.js --type=Vendor --vendorPort=8080"
-               ssh -n $target "lightningd --network=regtest --log-level=debug --daemon && sleep 5 && pkill lightning && node /home/$user/patching-lightning/Utils/generateAddress.js --hsmSecretPath=/home/nachiket/.lightning/hsm_secret --configFilePath=/home/nachiket/patching-lightning/Vendor/Vendor_config.json && sudo rm -r ~/.lightning/ && lightningd --network=regtest --log-level=debug --daemon >> runLog.log 2>&1 &"
+               ssh -n $target "lightningd --network=regtest --log-level=debug --daemon && sleep 5 && pkill lightning && node /home/$user/patching-lightning/Utils/generateAddress.js --hsmSecretPath=/home/$user/.lightning/hsm_secret --configFilePath=/home/$user/patching-lightning/Vendor/Vendor_config.json && sudo rm -r ~/.lightning/ && lightningd --network=regtest --log-level=debug --daemon >> runLog.log 2>&1 &"
            fi
 
            echo "End of installation $ip"
